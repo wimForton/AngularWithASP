1e10048530b3cb3dea472e877a9f2f2205c6b0c5
wim forton <wim.forton@gmail.com>
2024-10-10 13:56:36 +0200

    Sprite material

--- angularwithasp.client/src/app/components/application3D/3Dtools/ParticleSystem/ParticleScene.ts
+++ angularwithasp.client/src/app/components/application3D/3Dtools/ParticleSystem/ParticleScene.ts
@@ -6,6 +6,12 @@ export class ParticleScene {
   public scene: THREE.Scene = new THREE.Scene();
   private particleSystem: ParticleSystem;
   private ThreeParticles: THREE.Object3D[] = [];
+  //private vertices: Float32Array = new Float32Array();
+  private vertices: Array<number> = [];
+  private sizes: Array<number> = [];
+  private threeSpriteParticles: THREE.Points = new THREE.Points();
+  private threeSprites: Array<THREE.Sprite> = [];
+  private spritegeometry = new THREE.BufferGeometry();
   constructor(particleSystem: ParticleSystem) {
     this.particleSystem = particleSystem;
     this.CreateSceneLights();
@@ -36,7 +42,38 @@ export class ParticleScene {
   }
 
   private CreateThreeObjects() {
+    //const spritegeometry = new THREE.BufferGeometry();
+
+    const textureLoader = new THREE.TextureLoader();
+    //const assignSRGB = (texture: { colorSpace: string; }) => {texture.colorSpace = THREE.SRGBColorSpace;};
+    //const sprite = textureLoader.load('textures/sprites/snowflake1.png', assignSRGB);
+    const sprite = textureLoader.load('textures/snowflake2.png');
+    sprite.colorSpace = THREE.SRGBColorSpace;
+    const size = 5;
+    const color = [0.3, 0.8, 0.5];
+    const material = new THREE.PointsMaterial({ size: size, map: sprite, blending: THREE.AdditiveBlending, depthTest: false, transparent: true });
+    material.color.setHSL(color[0], color[1], color[2], THREE.SRGBColorSpace);
+    const spriteMaterial = new THREE.SpriteMaterial({ map: sprite, blending: THREE.AdditiveBlending, transparent: true, color: 0xffffff, fog: true });
+    spriteMaterial.color.setHSL(color[0], color[1], color[2], THREE.SRGBColorSpace);
+
+
     for (var p = 0; p < this.particleSystem.Particles.length; p++) {
+      this.vertices.push(0, 0, 0);
+      this.sizes.push(0.1);
+
+      const sprite = new THREE.Sprite(spriteMaterial);
+      this.scene.add(sprite);
+      this.threeSprites.push(sprite);
+    }
+    this.spritegeometry.setAttribute('position', new THREE.Float32BufferAttribute(this.vertices, 3));
+    this.spritegeometry.setAttribute('size', new THREE.Float32BufferAttribute(this.sizes, 1));
+    this.threeSpriteParticles = new THREE.Points(this.spritegeometry, material);
+    //this.scene.add(this.threeSpriteParticles);
+    //this.scene.add(this.threeSprites);
+
+    for (var p = 0; p < this.particleSystem.Particles.length; p++) {
+
+
 
       const material = new THREE.MeshStandardMaterial({ color: new THREE.Color("rgb(20, 200, 200)"), wireframe: false });
       //const geometry = new THREE.TorusGeometry(1, 0.3, 90, 50);
@@ -47,7 +84,7 @@ export class ParticleScene {
       );
       //torus.scale.set(1, 1, 1);
       this.ThreeParticles.push(sphere);
-      this.scene.add(sphere);
+      //this.scene.add(sphere);
 
     }
   }
@@ -55,9 +92,24 @@ export class ParticleScene {
   public Update() {
     this.particleSystem.SimulateFrame();
     for (var p = 0; p < this.particleSystem.Particles.length; p++) {
+
+
+
       let particle = this.particleSystem.Particles[p];
       this.ThreeParticles[p].scale.set(particle.scale.x, particle.scale.y, particle.scale.z);
       this.ThreeParticles[p].position.set(particle.position.x, particle.position.y, particle.position.z);
+
+      const vectorIndexStart = p * 3;
+
+      this.vertices[vectorIndexStart] = particle.position.x;
+      this.vertices[vectorIndexStart + 1] = particle.position.y;
+      this.vertices[vectorIndexStart + 2] = particle.position.z;
+      this.sizes[p] = particle.scale.x;
+      this.spritegeometry.setAttribute('position', new THREE.Float32BufferAttribute(this.vertices, 3));
+      this.spritegeometry.setAttribute('size', new THREE.Float32BufferAttribute(this.sizes, 1));
+      //this.spritegeometry.attributes.position.needsUpdate = true;
+      this.threeSprites[p].position.set(particle.position.x, particle.position.y, particle.position.z);
+      this.threeSprites[p].scale.set(particle.scale.x, particle.scale.y, particle.scale.z);
     }
   }
 
